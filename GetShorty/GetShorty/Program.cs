034592e6cc22f19f7548d1944bb15d99aee3d0c3
Spudgy <pcjoe3343@gmail.com>
2017-02-24 02:34:38 -0700

    Commiting before I fuck it up

--- GetShorty/GetShorty/Program.cs
+++ GetShorty/GetShorty/Program.cs
@@ -10,7 +10,15 @@ namespace GetShorty
     {
         static void Main(string[] args)
         {
+            Graph g = new Graph(3);
 
+            g.addEdge(0, 1, 0.9f);
+            g.addEdge(1, 2, 0.9f);
+            g.addEdge(0, 2, 0.8f);
+
+            g.printGraph();
+
+            Console.Read();
         }
 
         class Graph
@@ -22,16 +30,61 @@ namespace GetShorty
                 adjacencyList = new LinkedList<Tuple<int, float>>[intersections];
             }
 
-            public void addIntersections(int val)
-            {
-
-            }
-
             public void addEdge(int x, int y, float f)
             {
+                if(adjacencyList[x] == null)
+                    adjacencyList[x] = new LinkedList<Tuple<int, float>>();
 
+                if (adjacencyList[y] == null)
+                    adjacencyList[y] = new LinkedList<Tuple<int, float>>();
+
+                Tuple<int, float> intersection1 = new Tuple<int, float>(y, f);
+                adjacencyList[x].AddLast(intersection1);
+
+                Tuple<int, float> intersection2 = new Tuple<int, float>(x, f);
+                adjacencyList[y].AddLast(intersection2);
+            }
+
+            public void printGraph()
+            {
+                int count = 0;
+                foreach (LinkedList<Tuple<int, float>> intersection in adjacencyList)
+                {
+                    Console.Write("[" + count + "]");
+                    foreach (Tuple<int, float> edge in intersection)
+                    {
+                        Console.Write(" --> " + " <" + edge.Item1 + ", " + edge.Item2 + "> ");
+                    }
+                    Console.WriteLine();
+                    count++;
+                }
+            }
+
+            public void findBestPath()
+            {
+                int[] prev;
+                int[] weight;
+
+                for(int i = 0; i < adjacencyList.Count(); i++)
+                {
+                    //prev[i] = -1;
+                    //weight[i] = null;
+                }
             }
         }
 
     }
+
+    class Intersection
+    {
+        public bool visited;
+        public int number;
+
+        public Intersection(bool visited, int number)
+        {
+            this.visited = visited;
+            this.number = number;
+        }
+    }
+
 }
