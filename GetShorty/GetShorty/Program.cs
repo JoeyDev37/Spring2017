69f55b9e86e1d2d5b61083cb66185399acbeca69
Spudgy <pcjoe3343@gmail.com>
2017-02-24 01:01:20 -0700

    Set up skeleton for GetShorty

---
+++ GetShorty/GetShorty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetShorty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Graph
+        {
+            LinkedList<Tuple<int, float>>[] adjacencyList;
+
+            public Graph(int intersections)
+            {
+                adjacencyList = new LinkedList<Tuple<int, float>>[intersections];
+            }
+
+            public void addIntersections(int val)
+            {
+
+            }
+
+            public void addEdge(int x, int y, float f)
+            {
+
+            }
+        }
+
+    }
+}
