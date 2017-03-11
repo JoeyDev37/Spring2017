352b7e8905f5818466d3bee09f31cfd7ec9713b0
Spudgy <pcjoe3343@gmail.com>
2017-03-10 17:37:55 -0700

    update

--- NumberTheory/NumberTheory/Program.cs
+++ NumberTheory/NumberTheory/Program.cs
@@ -16,7 +16,7 @@ namespace NumberTheory
             //    string[] tokens = s.Split(' ');
 
             //}
-            Console.WriteLine(gcd(2345527849052734077, 2348570293847502555));
+            Console.WriteLine(isPrime(13));
             Console.Read();
         }
 
@@ -33,25 +33,57 @@ namespace NumberTheory
             return (long)result; 
         }
 
-        //private static string inverse(int a, int b)
-        //{
-            
-        //}
-        //private static bool isPrime(int a)
-        //{
-
-        //}
+        private static string inverse(long a, long N)
+        {
+            long[] result = ee(a, N);
+            if (result[2] == 1)
+                return (result[0] % N).ToString();
+            else
+                return "none";
+        }
+        private static string isPrime(long N)
+        {
+            if (N == 1)
+            { // 1 isn't prime
+                return "no";
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                // choose a random integer between 1 and p-1 ( inclusive )
+                long a = rand() % (p - 1) + 1;
+                // modulo is the function we developed above for modular exponentiation.
+                if (modulo(a, p - 1, p) != 1)
+                {
+                    return false; /* p is definitely composite */
+                }
+            }
+            return true; /* p is probably prime */
+        }
         //private static int key(int val1, int val2)
         //{
 
         //}
 
-        //private long[] ee(long a, long b)
-        //{
-        //    long d = gcd(a, b);
-        //    long[] result;
-        //    result = ee(b, a);
-        //    result =
-        //}
+        private static long[] ee(long a, long b)
+        {
+            long[] result = new long[3];
+            if (b == 0)
+            {
+                result[0] = 1;
+                result[1] = 0;
+                result[2] = a;
+                return result;
+            }
+            else
+            {
+                long[] result2 = ee(b, a % b);
+                result[0] = result2[1];
+                result[1] = result2[0] - (a / b) * result2[1];
+                result[2] = result2[2];
+                return result;
+            }
+        }  
     }
+
+    
 }
